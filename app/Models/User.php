9fc9753e987cb37bb5fc378b2ab71b70dc3f1d72
unknown <halaaa.abdelaziz95@gmail.com>
2022-03-29 10:31:05 +0200

    creating migrations and models of tables then adding relation ship between user and his posts

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Post;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone'
     ];
 
     /**
@@ -41,4 +42,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function post(){
+        return $this->hasMany(Post::class);
+    }
 }
--- database/migrations/2022_03_29_080848_create_posts_table.php
+++ database/migrations/2022_03_29_080848_create_posts_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('body');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
